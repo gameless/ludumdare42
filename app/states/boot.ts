393614ee85a0375d9b2db28dc1eb3ecf5f3421e7
Sam Estep <sam@samestep.com>
2018-08-11 10:53:11 -0400

    Make it a tad smaller
    (pesky scrollbars)

--- app/states/boot.ts
+++ app/states/boot.ts
@@ -2,7 +2,7 @@ export default function(game: Phaser.Game) {
   return {
     create() {
       game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-      game.scale.setUserScale(8, 8);
+      game.scale.setUserScale(7, 7);
       game.renderer.renderSession.roundPixels = true;
       Phaser.Canvas.setImageRenderingCrisp(game.canvas);
 
