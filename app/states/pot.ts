08dabde505bb613010392dd51b89a82bf95987ad
Sam Estep <sam@samestep.com>
2018-08-11 14:14:31 -0400

    Reveal pot interior when mouse gets close

--- app/states/pot.ts
+++ app/states/pot.ts
@@ -1,4 +1,16 @@
 export default function(game: Phaser.Game) {
+  const closeToPot = new Phaser.Polygon([
+    [37, 44],
+    [50, 81],
+    [67, 86],
+    [89, 86],
+    [105, 80],
+    [114, 44],
+    [100, 33],
+    [50, 33]
+  ].map(([x, y]) => new Phaser.Point(x, y)));
+  let pot: Phaser.Image;
+
   return {
     create() {
       game.sound.play('pot_music', 1, true);
@@ -7,9 +19,15 @@ export default function(game: Phaser.Game) {
       game.add.image(0, 0, 'shelf');
       game.add.image(0, 0, 'pot_cross');
       game.add.image(0, 0, 'root');
-      game.add.image(0, 0, 'pot');
+      pot = game.add.image(0, 0, 'pot');
       game.add.image(0, 0, 'plant');
       game.add.image(0, 0, 'highlight');
+    },
+
+    render() {
+      const mouseX = game.input.mousePointer.x;
+      const mouseY = game.input.mousePointer.y;
+      pot.alpha = closeToPot.contains(mouseX, mouseY) ? 0 : 1;
     }
   };
 };
