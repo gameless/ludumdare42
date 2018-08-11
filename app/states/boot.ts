5cd1eba8634c065298a2da45ff338d27512a34e5
Sam Estep <sam@samestep.com>
2018-08-11 12:53:00 -0400

    Draw pot scene

--- app/states/boot.ts
+++ app/states/boot.ts
@@ -5,6 +5,14 @@ export default function(game: Phaser.Game) {
         'Audio/Music/LD42Fun1.mp3',
         'Audio/Music/LD42Fun1.ogg'
       ]);
+
+      game.load.image('background', 'Image/scene1/background1withplants.png');
+      game.load.image('shelf', 'Image/scene1/shelf.png');
+      game.load.image('pot_cross', 'Image/scene1/pottransparent.png');
+      game.load.image('root', 'Image/scene1/root1.png');
+      game.load.image('pot', 'Image/scene1/pot.png');
+      game.load.image('plant', 'Image/scene1/plant1.png');
+      game.load.image('highlight', 'Image/scene1/highlighting.png');
     },
 
     create() {
--- app/states/pot.ts
+++ app/states/pot.ts
@@ -1,12 +1,15 @@
 export default function(game: Phaser.Game) {
   return {
     create() {
-      const graphics = game.add.graphics();
-      graphics.beginFill(0xffa500);
-      graphics.drawCircle(100, 75, 100);
-      graphics.endFill();
-
       game.sound.play('pot_music', 1, true);
+
+      game.add.image(0, 0, 'background');
+      game.add.image(0, 0, 'shelf');
+      game.add.image(0, 0, 'pot_cross');
+      game.add.image(0, 0, 'root');
+      game.add.image(0, 0, 'pot');
+      game.add.image(0, 0, 'plant');
+      game.add.image(0, 0, 'highlight');
     }
   };
 };
