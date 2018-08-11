41f0b18bc8ae8b335ee1386ef3f8c22f4badde46
Sam Estep <sam@samestep.com>
2018-08-11 14:38:59 -0400

    Smooooth

--- app/states/pot.ts
+++ app/states/pot.ts
@@ -1,15 +1,8 @@
 export default function(game: Phaser.Game) {
-  const closeToPot = new Phaser.Polygon([
-    [37, 44],
-    [50, 81],
-    [67, 86],
-    [89, 86],
-    [105, 80],
-    [114, 44],
-    [100, 33],
-    [50, 33]
-  ].map(([x, y]) => new Phaser.Point(x, y)));
   let pot: Phaser.Image;
+  let fade: Phaser.Tween;
+  let hover: Phaser.Signal;
+  let hovering = false;
 
   return {
     create() {
@@ -22,12 +15,38 @@ export default function(game: Phaser.Game) {
       pot = game.add.image(0, 0, 'pot');
       game.add.image(0, 0, 'plant');
       game.add.image(0, 0, 'highlight');
+
+      hover = new Phaser.Signal();
+      hover.add(function() {
+        const newAlpha = hovering ? 0 : 1;
+        const time = Math.abs(newAlpha - pot.alpha) * 250;
+        if (fade) {
+          fade.stop();
+        }
+        fade = game.add.tween(pot);
+        fade.to({ alpha: newAlpha }, time, Phaser.Easing.Default, true);
+      });
     },
 
     render() {
+      const closeToPot = new Phaser.Polygon([
+        [37, 44],
+        [50, 81],
+        [67, 86],
+        [89, 86],
+        [105, 80],
+        [114, 44],
+        [100, 33],
+        [50, 33]
+      ].map(([x, y]) => new Phaser.Point(x, y)));
+
       const mouseX = game.input.mousePointer.x;
       const mouseY = game.input.mousePointer.y;
-      pot.alpha = closeToPot.contains(mouseX, mouseY) ? 0 : 1;
+      const hoveringNow = closeToPot.contains(mouseX, mouseY);
+      if (hoveringNow !== hovering) {
+        hovering = hoveringNow;
+        hover.dispatch();
+      }
     }
   };
 };
