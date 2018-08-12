549b8bbe15dd50e0542701f39fc6f28539130d79
Sam Estep <sam@samestep.com>
2018-08-11 21:38:46 -0400

    Build

--- app.js
+++ app.js
@@ -167,17 +167,17 @@ require.register("states/boot.ts", function(exports, require, module) {
 function default_1(game) {
     return {
         preload: function () {
-            game.load.audio('music1', 'Audio/Music/LD42Fun1.ogg');
-            game.load.audio('music2', 'Audio/Music/LD42Fun2.ogg');
-            game.load.audio('music3', 'Audio/Music/LD42Fun3.ogg');
-            game.load.audio('root1', 'Audio/SoundEffects/LD42RootGrow1.ogg');
-            game.load.audio('root2', 'Audio/SoundEffects/LD42RootGrow2.ogg');
-            game.load.audio('root3', 'Audio/SoundEffects/LD42RootGrow3.ogg');
-            game.load.audio('root4', 'Audio/SoundEffects/LD42RootGrow4.ogg');
-            game.load.audio('root5', 'Audio/SoundEffects/LD42RootGrow5.ogg');
-            game.load.audio('root6', 'Audio/SoundEffects/LD42RootGrow6.ogg');
-            game.load.audio('root7', 'Audio/SoundEffects/LD42RootGrow7.ogg');
-            game.load.audio('shatter', 'Audio/SoundEffects/LD42PotShattering.ogg');
+            game.load.audio('music1', 'Audio/Music/Fun1.ogg');
+            game.load.audio('music2', 'Audio/Music/Fun2.ogg');
+            game.load.audio('music3', 'Audio/Music/Fun3.ogg');
+            game.load.audio('root1', 'Audio/SoundEffects/RootGrow1.ogg');
+            game.load.audio('root2', 'Audio/SoundEffects/RootGrow2.ogg');
+            game.load.audio('root3', 'Audio/SoundEffects/RootGrow3.ogg');
+            game.load.audio('root4', 'Audio/SoundEffects/RootGrow4.ogg');
+            game.load.audio('root5', 'Audio/SoundEffects/RootGrow5.ogg');
+            game.load.audio('root6', 'Audio/SoundEffects/RootGrow6.ogg');
+            game.load.audio('root7', 'Audio/SoundEffects/RootGrow7.ogg');
+            game.load.audio('shatter', 'Audio/SoundEffects/PotShattering.ogg');
             game.load.image('pot_bg', 'Image/scene1/blurredbg.png');
             game.load.image('pot_shelf', 'Image/scene1/shelf.png');
             game.load.image('pot_shelf_hl', 'Image/scene1/shelfhighlighting.png');
@@ -203,7 +203,7 @@ function default_1(game) {
             game.load.spritesheet('pot_pot', 'Image/scene1/potbreaking spritesheet.png', 160, 90);
             game.load.spritesheet('pot_rootleft', 'Image/scene1/rootleft spritesheet.png', 160, 90);
             game.load.spritesheet('pot_rootright', 'Image/scene1/rootright spritesheet.png', 160, 90);
-            game.load.audio('thud', 'Audio/SoundEffects/LD42Thud.ogg');
+            game.load.audio('thud', 'Audio/SoundEffects/Thud.ogg');
             game.load.image('room_bg', 'Image/scene2/background2.png');
             game.load.image('room_int', 'Image/scene2/wallinterior.png');
             game.load.image('room_pot', 'Image/scene2/brokenpotshards.png');
@@ -222,7 +222,7 @@ function default_1(game) {
             musics['1'] = game.sound.play('music1', 1, true);
             musics['2'] = game.sound.play('music2', 0, true);
             musics['3'] = game.sound.play('music3', 0, true);
-            game.state.start('pot', true, false, musics);
+            game.state.start('room', true, false, musics);
         }
     };
 }
@@ -416,7 +416,7 @@ function default_1(game) {
             hover = new Phaser.Signal();
             var timer = game.time.create();
             timer.add(1000, function () {
-                game.sound.play('thud');
+                game.sound.play('thud', 4);
                 hover.add(function () {
                     var newAlpha = hovering ? 0 : 1;
                     var time = Math.abs(newAlpha - wall.alpha) * 250;
