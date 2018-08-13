265e1a1366b89f4ea29ab608f72b95387302a62a
Sam Estep <sam@samestep.com>
2018-08-13 15:40:17 -0400

    Add switch for menu screen

--- app/states/menu.ts
+++ app/states/menu.ts
@@ -1,8 +1,16 @@
 import { Highlight } from '../highlight';
 import { Music, MusicalState, startState } from '../music';
 
-const button = new Phaser.Polygon([
+const startButton = new Phaser.Polygon([
   [16, 25], [13, 57], [145, 64], [145, 30]
+].map(([x, y]) => new Phaser.Point(x, y)));
+
+const resumeButton = new Phaser.Polygon([
+  [17, 6], [14, 38], [146, 45], [146, 11]
+].map(([x, y]) => new Phaser.Point(x, y)));
+
+const restartButton = new Phaser.Polygon([
+  [14, 46], [14, 80], [143, 85], [146, 53]
 ].map(([x, y]) => new Phaser.Point(x, y)));
 
 export default class extends MusicalState {
@@ -23,16 +31,39 @@ export default class extends MusicalState {
     this.music.fadeTrack(500, 'title');
 
     this.game.add.image(0, 0, 'menu_background');
-    const buttonSprite = this.game.add.sprite(0, 0, 'menu_start');
+    const start = (this.behind === 'pot');
+    const key = start ? 'menu_start' : 'menu_resume';
+    const buttonSprite = this.game.add.sprite(0, 0, key);
 
     this.highlight = new Highlight(this.game, (x, y) => {
-      return button.contains(x, y) ? buttonSprite : null;
+      if (start) {
+        if (startButton.contains(x, y)) {
+          return buttonSprite;
+        }
+      } else {
+        if (resumeButton.contains(x, y) || restartButton.contains(x, y)) {
+          return buttonSprite;
+        }
+      }
+      return null;
     });
 
     this.game.input.onUp.add(() => {
-      if (button.contains(this.game.input.x, this.game.input.y)) {
-        this.highlight.destroy();
-        startState(this.game, this.behind, this.music);
+      const x = this.game.input.x;
+      const y = this.game.input.y;
+      if (start) {
+        if (startButton.contains(x, y)) {
+          this.highlight.destroy();
+          startState(this.game, this.behind, this.music);
+        }
+      } else {
+        if (resumeButton.contains(x, y)) {
+          this.highlight.destroy();
+          startState(this.game, this.behind, this.music);
+        } else if (restartButton.contains(x, y)) {
+          this.highlight.destroy();
+          startState(this.game, 'pot', this.music);
+        }
       }
     });
   }
